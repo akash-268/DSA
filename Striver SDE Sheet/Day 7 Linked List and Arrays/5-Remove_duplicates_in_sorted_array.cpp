b610f29679a420cdda5f88e4a176cfe9022451f1
akash-268 <aakbajpai268@gmail.com>
2022-08-10 00:22:57 +0530

    remove duplicates

--- Striver SDE Sheet/Day 7 Linked List and Arrays/5-Remove_duplicates_in_sorted_array.cpp
+++ Striver SDE Sheet/Day 7 Linked List and Arrays/5-Remove_duplicates_in_sorted_array.cpp
@@ -3,14 +3,6 @@ using namespace std;
 typedef long long int ll;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 int removeDuplicates(vector<int> &nums)
 {
     // map<int, int> mp;
