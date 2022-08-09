e8647d71524831d69704b1942d63e84fe2c15801
akash-268 <aakbajpai268@gmail.com>
2022-08-09 11:34:16 +0530

    rotate list

---
+++ Striver SDE Sheet/Day 7 Linked List and Arrays/1-Rotate_list_k_times.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *rotateRight(ListNode *head, int k)
+{
+    if (!head || !head->next)
+        return head;
+    while (k--)
+    {
+        ListNode *nex = head->next;
+        ListNode *cur = head;
+        while (nex->next)
+        {
+            cur = nex;
+            nex = nex->next;
+        }
+        nex->next = head;
+        cur->next = NULL;
+        head = nex;
+    }
+    return head;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    while (t--)
+    {
+        solve();
+    }
+}
