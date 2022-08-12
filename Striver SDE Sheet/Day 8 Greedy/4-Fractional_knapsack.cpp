65ba5e4f1bc0279e90c8bfe8363d4a0629be534f
akash-268 <aakbajpai268@gmail.com>
2022-08-13 02:03:24 +0530

    fractional knapsack

---
+++ Striver SDE Sheet/Day 8 Greedy/4-Fractional_knapsack.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin                    \
+    for (ll i = 0; i < n; i++) \
+    cin >> v[i]
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct Item
+{
+    int value;
+    int weight;
+};
+bool static comp(Item a, Item b)
+{
+    double r1 = (double)a.value / (double)a.weight;
+    double r2 = (double)b.value / (double)b.weight;
+    return r1 > r2;
+}
+// function to return fractionalweights
+double fractionalKnapsack(int W, Item arr[], int n)
+{
+    sort(arr, arr + n, comp);
+    int curWeight = 0;
+    double finalvalue = 0.0;
+    for (int i = 0; i < n; i++)
+    {
+        if (curWeight + arr[i].weight <= W)
+        {
+            curWeight += arr[i].weight;
+            finalvalue += arr[i].value;
+        }
+        else
+        {
+            int remain = W - curWeight;
+            finalvalue += (arr[i].value / (double)arr[i].weight) * (double)remain;
+            break;
+        }
+    }
+    return finalvalue;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
