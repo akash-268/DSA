96fbefb12387b411114e0281c730a848e048d093
akash-268 <aakbajpai268@gmail.com>
2022-08-12 23:45:00 +0530

    543

---
+++ Leetcode Imp/543-Diameter_of_binary_tree.cpp
@@ -0,0 +1,72 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin                    \
+    for (ll i = 0; i < n; i++) \
+    cin >> v[i]
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int fun(TreeNode *root, int maxi)
+{
+    if (!root)
+        return 0;
+    int lh = fun(root->left, maxi);
+    int rh = fun(root->right, maxi);
+    maxi = max(maxi, lh + rh);
+    return 1 + max(lh, rh);
+}
+int diameterOfBinaryTree(TreeNode *root)
+{
+    int maxi = 0;
+    fun(root, maxi);
+    return maxi;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
