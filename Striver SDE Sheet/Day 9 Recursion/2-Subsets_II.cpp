05e8dcbcf52482205b3536db612261d8953b99cb
akash-268 <aakbajpai268@gmail.com>
2022-08-14 12:38:28 +0530

    subsets 2

--- Striver SDE Sheet/Day 9 Recursion/2-Subsets_II.cpp
+++ Striver SDE Sheet/Day 9 Recursion/2-Subsets_II.cpp
@@ -3,46 +3,6 @@ using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
 #define ll long long int
-#define vin                    \
-    for (ll i = 0; i < n; i++) \
-    cin >> v[i]
-#define f(i, a, b) for (ll i = a; i < b; i++)
-#define rf(i, a, b) for (ll i = a; i >= b; i--)
-#define mp make_pair
-#define pb push_back
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define mll map<ll, ll>
-#define vc vector
-#define fi first
-#define se second
-#define all(x) x.begin(), x.end()
-const int maxi = 1e5 + 5;
-const ll mod = 1e9 + 7;
-const ll inf = 1e9;
-const ll eps = 1e-9;
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-#include <bits/stdc++.h>
-using namespace std;
-// JAI SHREE RAM
-// HAR HAR MAHADEV
-#define ll long long int
 #define vin vector<int>
 #define f(i, a, b) for (ll i = a; i < b; i++)
 #define rf(i, a, b) for (ll i = a; i >= b; i--)
@@ -102,22 +62,28 @@ vector<vector<int>> subsetsWithDup(vector<int> &nums)
 }
 void solve()
 {
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ll t = 1;
-    // cin >> t;
-    while (t--)
+    int n;
+    cin >> n;
+    vin v(n);
+    f(i, 0, n)
     {
-        solve();
+        cin >> v[i];
     }
-}
-void solve()
-{
+    vector<vector<int>> res = subsetsWithDup(v);
+    for (auto it : res)
+    {
+        cout << "[";
+        for (int i = 0; i < it.size(); i++)
+        {
+            if (i == it.size() - 1)
+            {
+                cout << it[i];
+            }
+            else
+                cout << it[i] << ",";
+        }
+        cout << "] ";
+    }
 }
 
 int main()
