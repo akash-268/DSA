78b8bc6b969ef6e65582e651843661276a5c9dd3
akash-268 <aakbajpai268@gmail.com>
2022-08-14 16:47:51 +0530

    103

--- Leetcode Imp/Tree/103-zigzag_traversal_binary_tree.cpp
+++ Leetcode Imp/Tree/103-zigzag_traversal_binary_tree.cpp
@@ -36,6 +36,34 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+{
+    vector<vector<int>> res; // answer vector
+    queue<TreeNode *> q;     // helper queue
+    if (!root)
+        return res;
+    q.push(root);
+    bool flag = true; // true matlab left tot right, false matlab right to left
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> temp(size);
+        for (int i = 0; i < size; i++) // size tak chalaya to get the level by level elements
+        {
+            TreeNode *node = q.front();
+            q.pop();
+            int id = flag ? i : size - 1 - i; // left to right store karna ya phir right to left using simple maths
+            temp[id] = node->val;
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        flag = !flag;        // har level ke khatam hone pe flag ko revert karna (bohot important)
+        res.push_back(temp); // har level ko push back karate gaye answer mein
+    }
+    return res;
+}
 void solve()
 {
 }
