e08978a15f97df0cf22c4908e6dec04bcead3787
akash-268 <aakbajpai268@gmail.com>
2022-08-15 15:32:32 +0530

    kth permutation

--- Striver SDE Sheet/Day 9 Recursion/6-Kth_permutation.cpp
+++ Striver SDE Sheet/Day 9 Recursion/6-Kth_permutation.cpp
@@ -36,8 +36,27 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+string getPermutation(int n, int k)
+{
+    vector<int> v;
+    for (int i = 1; i <= n; i++)
+        v.push_back(i);
+
+    for (int i = 1; i < k; i++)
+    {
+        next_permutation(v.begin(), v.end());
+    }
+    string res = "";
+    for (auto it : v)
+    {
+        res += it + '0';
+    }
+    return res;
+}
 void solve()
 {
+    int n,k;cin>>n>>k;
+    cout<<getPermutation(n,k);
 }
 
 int main()
