03d72c5b0475f3f9576d82ed119302da934b49b7
akash-268 <aakbajpai268@gmail.com>
2022-08-17 00:31:08 +0530

    sudoku solver

--- Striver SDE Sheet/Day 10 Recursion-Backtracking/3-Sudoku_solver.cpp
+++ Striver SDE Sheet/Day 10 Recursion-Backtracking/3-Sudoku_solver.cpp
@@ -36,8 +36,51 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+bool possible(vector<vector<char>> &board, int row, int col, int k)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[row][i] == k)
+            return false;
+        if (board[i][col] == k)
+            return false;
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == k)
+            return false;
+    }
+    return true;
+}
+bool solve(vector<vector<char>> &board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            if (board[i][j] == '.')
+            {
+                for (char k = '1'; k <= '9'; k++)
+                {
+                    if (possible(board, i, j, k))
+                    {
+                        board[i][j] = k;
+                        if (solve(board) == true)
+                            return true;
+                        else
+                            board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+void solveSudoku(vector<vector<char>> &board)
+{
+    solve(board);
+}
 void solve()
 {
+    
 }
 
 int main()
