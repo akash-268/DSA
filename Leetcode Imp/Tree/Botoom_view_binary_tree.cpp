3553c4f9462d7960b6045a7bf6fbef2966658675
akash-268 <aakbajpai268@gmail.com>
2022-08-17 23:17:20 +0530

    top view of binary tree

---
+++ Leetcode Imp/Tree/Botoom_view_binary_tree.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin vector<int>
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+vector<int> getTopView(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> ans;
+    if (!root)
+        return ans;
+    queue<pair<TreeNode *, int>> q;
+    map<int, int> mp;
+    q.push({root, 0});
+    while (!q.empty())
+    {
+        TreeNode *node = q.front().first;
+        int line = q.front().second;
+        q.pop();
+        mp[line] = node->val;
+        if (node->left)
+        {
+            q.push({node->left, line - 1});
+        }
+        if (node->right)
+        {
+            q.push({node->right, line + 1});
+        }
+    }
+    for (auto it : mp)
+    {
+        ans.push_back(it.second);
+    }
+    return ans;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
