8b6e24e010a244ad7d2c32502f251b6e41803734
akash-268 <aakbajpai268@gmail.com>
2022-08-23 00:10:17 +0530

    children sum property

--- Leetcode Imp/Tree/Children_sum_property.cpp
+++ Leetcode Imp/Tree/Children_sum_property.cpp
@@ -36,6 +36,25 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+void changeTree(TreeNode* root){
+    if(!root) return;
+    int child=0;
+    if(root->left)
+        child += root->left->val;
+    if (root->right)
+        child += root->right->val;
+    if(child>=root->val) root->val=child;
+    else{
+        if(root->left) root->left->val=root->val;
+        if(root->right) root->right->val=root->val;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+    int sum=0;
+    if(root->left) sum+=root->left->val;
+    if(root->right) sum+=root->right->val;
+    if(root->left or root->right) root->val=sum;
+}
 void solve()
 {
 }
