17218d0029fca7ae8a06c27134bb8ded4a28598c
akash-268 <aakbajpai268@gmail.com>
2022-08-23 23:11:06 +0530

    863

---
+++ Leetcode Imp/Tree/863-Nodes_at_distance_k.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin vector<int>
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+void markParents(TreeNode *root, unordered_map<TreeNode *, TreeNode *> &parent_track, TreeNode *target)
+{
+    queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty())
+    {
+        TreeNode *current = queue.front();
+        queue.pop();
+        if (current->left)
+        {
+            parent_track[current->left] = current;
+            queue.push(current->left);
+        }
+        if (current->right)
+        {
+            parent_track[current->right] = current;
+            queue.push(current->right);
+        }
+    }
+}
+vector<int> distanceK(TreeNode *root, TreeNode *target, int k)
+{
+    unordered_map<TreeNode *, TreeNode *> parent_track;
+    markParents(root, parent_track, target);
+    unordered_map<TreeNode *, bool> visited;
+    queue<TreeNode *> queue;
+    queue.push(target);
+    visited[target] = true;
+    int curr_level = 0;
+    while (!queue.empty())
+    {
+        int size = queue.size();
+        if (curr_level++ == k)
+            break;
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *current = queue.front();
+            queue.pop();
+            if (current->left && !visited[current->left])
+            {
+                queue.push(current->left);
+                visited[current->left] = true;
+            }
+            if (current->right && !visited[current->right])
+            {
+                queue.push(current->right);
+                visited[current->right] = true;
+            }
+            if (parent_track[current] && !visited[parent_track[current]])
+            {
+                queue.push(parent_track[current]);
+                visited[parent_track[current]] = true;
+            }
+        }
+    }
+    vector<int> result;
+    while (!queue.empty())
+    {
+        TreeNode *current = queue.front();
+        queue.pop();
+        result.push_back(current->val);
+    }
+    return result;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
--- temp.cpp
+++ temp.cpp
@@ -2,149 +2,42 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-#define ll long long int
-#define vin vector<int>
-#define f(i, a, b) for (ll i = a; i < b; i++)
-#define rf(i, a, b) for (ll i = a; i >= b; i--)
-#define mp make_pair
-#define pb push_back
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define mll map<ll, ll>
-#define vc vector
-#define fi first
-#define se second
-#define all(x) x.begin(), x.end()
-const int N = 1e5;
-vector<int> tree[N];
-int visited[N], baapu[N],
-    children[N], check = -1;
-
-void dfs(int curr, int papa)
+void check(vector<int> &v)
 {
-    baapu[curr] = papa;
-    for (int &child : tree[curr])
+    int c = 0, i;
+    for (i = 1; i < v.size(); i++)
     {
-
-        if (child == papa)
-            return;
-        dfs(child, curr);
+        if (v[i] == v[i - 1])
+            c++;
     }
-
-    if (visited[curr] == 1 && curr != 1)
-    {
-
-        children[curr] = 0;
-        return;
-    }
-
-    int total_child = 0;
-    for (auto &child : tree[curr])
-    {
-        if (child == papa)
-            return;
-        else
-            ++total_child;
-    }
-    children[curr] = total_child;
-    return;
-}
-
-int solve2(int n, int R)
-{
-    if (R > n)
-    {
-        check = 0;
-        return 0;
-    }
-    int npr = 1;
-    for (int i = n - R + 1; i <= n; ++i)
-    {
-        npr = npr * i;
-    }
-    return npr;
-}
-
-int NoOfWays(int Nodes, int colors)
-{
-
-    dfs(1, -1);
-    int ways = 0;
-    for (int i = 1; i <= Nodes; ++i)
-    {
-        if (i == 1)
-        {
-            ways = ways + colors *
-                              solve2(colors - 1, children[1]);
-        }
-        else
-        {
-            if (visited[i] == 1)
-            {
-                continue;
-            }
-            else
-            {
-                ways = ways *
-                       solve2(colors - 2, children[i]);
-            }
-        }
-    }
-    return ways;
-}
-
-void Tree(int n)
-{
-    while (n > 0)
-    {
-        int i, j;
-        cin >> i >> j;
-        tree[i].push_back(j);
-        tree[j].push_back(i);
-        visited[i]++, visited[j]++;
-        n--;
-    }
+    cout << v.size() - c << " ";
 }
 void solve()
 {
-    ll n, c;
-    cin >> n >> c;
-    Tree(n - 1);
-    dfs(1, -1);
-    ll ways = 0;
-    for (int i = 1; i <= n; ++i)
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    int i;
+    for (i = 0; i < n; i++)
     {
-        if (i == 1)
-        {
-            ways = ways + c *
-                              solve2(c - 1, children[1]);
-        }
-        else
-        {
-            if (visited[i] == 1)
-            {
-                continue;
-            }
-            else
-            {
-                ways = ways *
-                       solve2(c - 2, children[i]);
-            }
-        }
+        cin >> v[i];
     }
-    cout << ways << "\n";
+    vector<pair<int, int>> c;
+    for (i = 0; i < k; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        c.push_back(make_pair(x, y));
+    }
+    for (i = 0; i < c.size(); i++)
+    {
+        v[c[i].first - 1] = c[i].second;
+        check(v);
+    }
 }
+
 int main()
 {
-    // Your code is here
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ll t = 1;
-    // cin >> t;
-    while (t--)
-    {
-        solve();
-    }
+    solve();
     return 0;
 }
