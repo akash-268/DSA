0d1631eae212d70a3e1d8da0137a80d003b08160
akash-268 <aakbajpai268@gmail.com>
2022-08-24 00:06:53 +0530

    222

--- Leetcode Imp/Tree/222-total_nodes_in_complete_binary_tree.cpp
+++ Leetcode Imp/Tree/222-total_nodes_in_complete_binary_tree.cpp
@@ -36,6 +36,36 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+int left(TreeNode *node)
+{
+    int h = 0;
+    while (node)
+    {
+        h++;
+        node = node->left;
+    }
+    return h;
+}
+int right(TreeNode *node)
+{
+    int h = 0;
+    while (node)
+    {
+        h++;
+        node = node->right;
+    }
+    return h;
+}
+int countNodes(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int l = left(root);
+    int r = right(root);
+    if (l == r)
+        return (1 << l) - 1;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
 void solve()
 {
 }
