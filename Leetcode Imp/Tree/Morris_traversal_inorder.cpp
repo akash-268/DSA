fea338a830da188f7f6dde659cb33cccd2d8608b
akash-268 <aakbajpai268@gmail.com>
2022-08-25 01:18:23 +0530

    morris traversal

--- Leetcode Imp/Tree/Morris_traversal_inorder.cpp
+++ Leetcode Imp/Tree/Morris_traversal_inorder.cpp
@@ -36,6 +36,37 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> inorder;
+    TreeNode *cur = root;
+    while (cur)
+    {
+        if (!cur->left)
+        {
+            inorder.push_back(cur->val);
+            cur = cur->right;
+        }
+        else
+        {
+            TreeNode *pre = cur->left;
+            while (pre->right && pre->right != cur)
+                pre = pre->right;
+            if (!pre->right)
+            {
+                pre->right = cur;
+                cur = cur->left;
+            }
+            else
+            {
+                pre->right = NULL;
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+    }
+    return inorder;
+}
 void solve()
 {
 }
