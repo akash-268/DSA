28d91f7c3f6316266fabbca863f6db0114212cc7
akash-268 <aakbajpai268@gmail.com>
2022-08-25 16:19:57 +0530

    450

--- Leetcode Imp/Tree/Binary Search Tree/450-Delete_node_from_BST.cpp
+++ Leetcode Imp/Tree/Binary Search Tree/450-Delete_node_from_BST.cpp
@@ -36,6 +36,52 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+TreeNode *find(TreeNode *node)
+{
+    if (node->right == NULL)
+        return node;
+    node = node->right;
+}
+TreeNode *helper(TreeNode *root)
+{
+    if (root->left == NULL)
+        return root->right;
+    if (root->right == NULL)
+        return root->left;
+    TreeNode *rightchild = root->right;
+    TreeNode *lastright = find(root->left);
+    lastright->right = rightchild;
+    return root->left;
+}
+TreeNode *deleteNode(TreeNode *root, int key)
+{
+    // Write your code here.
+    if (root == NULL)
+        return root;
+    if (root->val == key)
+    {
+        return helper(root);
+    }
+    TreeNode *dummy = root;
+    while (root)
+    {
+        if (root->val < key)
+        {
+            if (root->right && root->right->val == key)
+                root->right = helper(root->right);
+            else
+                root = root->right;
+        }
+        else
+        {
+            if (root->left && root->left->val == key)
+                root->left = helper(root->left);
+            else
+                root = root->left;
+        }
+    }
+    return dummy;
+}
 void solve()
 {
 }
