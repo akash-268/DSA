755d47a6296c99d5b20dcaa05e5fb8de45ffed10
akash-268 <aakbajpai268@gmail.com>
2022-08-29 01:39:20 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -9,9 +9,9 @@ void solve()
 
 int main()
 {
-    auto b = make_shared<int>(25);
+    shared_ptr<int> b = make_shared<int>(25);
     cout << *b << endl;
-    auto x = make_unique<int[]>(10);
+    unique_ptr<int[]> x = make_unique<int[]>(10);
     for (int i = 0; i < 10; i++)
     {
         x[i] = i + 1;
