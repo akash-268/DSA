ae096bdb55f08c5107dea5ca7fc3c5c6a18f734c
akash-268 <aakbajpai268@gmail.com>
2022-08-28 21:14:17 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -2,64 +2,35 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-void mergeArrays(vector<int> arr1, vector<int> arr2, vector<int> &arr3, int n1, int n2)
-{
-    int i = 0, j = 0, k = 0;
-
-    // Traverse both array
-    while (i < n1 && j < n2)
-    {
-        if (arr1[i] < arr2[j])
-            arr3[k++] = arr1[i++];
-        else
-            arr3[k++] = arr2[j++];
-    }
-
-    // Store remaining elements of first array
-    while (i < n1)
-        arr3[k++] = arr1[i++];
-
-    // Store remaining elements of second array
-    while (j < n2)
-        arr3[k++] = arr2[j++];
-}
 
 void solve()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<int> v(n), v2(m);
-    set<int>st;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i];
-        st.insert(v[i]);
-    }
-    for (int i = 0; i < m; i++)
-    {
-        cin >> v2[i];
-        st.insert(v[i]);
-    }
-    sort(st.begin(),st.end());
-    vector<int>v3;
-    for(auto it:st){
-        v3.push_back(it);
-    }
-    int mid = st.size() / 2;
-    if (st.size()% 2)
-    {
-        float ans = v3[mid]/1.0;
-        cout << setprecision(2)<<ans;
-    }
-    else
-    {
-        float x = v3[mid], y = v3[mid - 1];
-        cout << setprecision(2)<<(x + y) / 2.0;
-    }
 }
 
 int main()
 {
-    solve();
+    // double i = 3.0;
+    // double *p1 = &i;
+    // int *ptr = new int[5];
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     *(ptr + i) = i + 1;
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << *(ptr + i) << " ";
+    // }
+    // cout << endl;
+    // cout << ptr + 1 << endl;
+    // double *p2 = p1;
+    // p1 = p1 + 1;
+    // cout << p2 - p1 << endl;
+    // cout << *p1;
+    // delete p1;
+    // delete ptr;
+    char *a[7] = "abcdef";
+    char *p1 = a;
+
+    cout << p1;
     return 0;
 }
--- template.cpp
+++ template.cpp
@@ -38,6 +38,7 @@ struct TreeNode
 };
 void solve()
 {
+    
 }
 
 int main()
