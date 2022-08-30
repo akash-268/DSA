8c4ea655943abd7d60cebb481a68849d461c313d
akash-268 <aakbajpai268@gmail.com>
2022-08-31 01:27:28 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -10,35 +10,52 @@ void solve()
 
 int main()
 {
-    stack<shared_ptr<int>> st;
-    auto x1 = make_shared<int>(1);
-    st.push(x1);
-    auto x2 = make_shared<int>(2);
-    st.push(x2);
-    auto x3 = make_shared<int>(3);
-    st.push(x3);
-    while (st.size() != 0)
+    // int *a = new int();
+    // int sum = 0;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     a[i] = i + 1;
+    //     sum += a[i];
+    // }
+    // cout << sum << endl;
+
+    string s = "a/b/c/d";
+    stringstream ss(s);
+    string word;
+    while (!ss.eof())
     {
-        cout << *st.top() << " ";
-        st.pop();
+        getline(ss, word, 'b');
+        cout << word << endl;
     }
-    cout << endl;
-    vector<shared_ptr<int>> vt;
-    auto y1 = make_shared<int>(1);
-    vt.push_back(y1);
-    auto y2 = make_shared<int>(2);
-    vt.push_back(y2);
-    auto y3 = make_shared<int>(3);
-    vt.push_back(y3);
-    for (auto &it : vt)
-        cout << *it << " ";
-    cout << endl;
+    // stack<shared_ptr<int>> st;
+    // auto x1 = make_shared<int>(1);
+    // st.push(x1);
+    // auto x2 = make_shared<int>(2);
+    // st.push(x2);
+    // auto x3 = make_shared<int>(3);
+    // st.push(x3);
+    // while (st.size() != 0)
+    // {
+    //     cout << *st.top() << " ";
+    //     st.pop();
+    // }
+    // cout << endl;
+    // vector<shared_ptr<int>> vt;
+    // auto y1 = make_shared<int>(1);
+    // vt.push_back(y1);
+    // auto y2 = make_shared<int>(2);
+    // vt.push_back(y2);
+    // auto y3 = make_shared<int>(3);
+    // vt.push_back(y3);
+    // for (auto &it : vt)
+    //     cout << *it << " ";
+    // cout << endl;
     // shared_ptr<int> b = make_shared<int>(25);
     // cout << *b << endl;
     // char str[] = "abcde";
     // unique_ptr<char[]> s = make_unique<char[]>(6);
-    // // s = str;
-    // // cout << s << endl;
+    // s = str;
+    // cout << s << endl;
     // unique_ptr<int[]> x = make_unique<int[]>(10);
     // for (int i = 0; i < 10; i++)
     // {
@@ -47,4 +64,4 @@ int main()
     // for (int i = 0; i < 10; i++)
     //     cout << x[i] << " ";
     return 0;
-} 
+}
--- temp2.cpp
+++ temp2.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-int binary_search(vector<int> &v, int k)
-{
-    int start = 0, end = v.size() - 1;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-        if (v[mid] == k)
-            return mid;
-        else if (v[mid] > k)
-            end = mid - 1;
-        else
-            start = mid + 1;
-    }
-    return end;
-}
 int main()
 {
 
@@ -27,7 +12,7 @@ int main()
         cin >> v[i];
     int k;
     cin >> k;
-    map<int, int> mp;
+    unordered_map<int, int> mp;
     for (auto it : v)
     {
         mp[it] = abs(k - it);
