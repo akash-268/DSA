4897ee8ad15a2e0c4acd0929defbf481c8792032
akash-268 <aakbajpai268@gmail.com>
2022-09-02 00:11:45 +0530

    FH

--- Commvault/temp.cpp
+++ Commvault/temp.cpp
@@ -17,11 +17,20 @@ void shuffle(vector<int> &arr)
 }
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    shuffle(arr);
-    print(arr);
-    shuffle(arr);
-    print(arr);
-    shuffle(arr);
-    print(arr);
+    vector<pair<int, int>> l;
+    l.push_back({1, 10});
+    l.push_back({2, 20});
+    l.push_back({3, 30});
+    l.push_back({4, 40});
+    l.push_back({5, 50});
+    map<int, int> mp;
+    mp.insert(1, 10);
+    mp.insert(2, 20);
+    mp.insert(3, 30);
+    mp.insert(4, 40);
+    mp.insert(5, 50);
+    auto i = mp.find(2);
+    vector<pair<int, int>>::iterator i1 = l.begin();
+    auto p1 = *(i1);
+    cout << p1.first << ":" << p1.second << endl;
 }
