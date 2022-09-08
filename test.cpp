e952a64300f1300fd5267ea62c93085d5f72f9a4
akash-268 <aakbajpai268@gmail.com>
2022-09-08 15:05:00 +0530

    dynamic object allocation

--- test.cpp
+++ test.cpp
@@ -1,26 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-class student
-{
-public:
-    string name;
-    int age;
-    int roll;
-    student(string s, int a, int r)
-    {
-        name = s;
-        age = a;
-        roll = r;
-    }
-};
 
 int main()
 {
-    student ob("akash", 22, 25636);
-    cout << &ob << endl
-         << ob.name << " " << ob.age << " " << ob.roll << endl;
-    student *ob2 = &ob;
-    cout << &ob2 << endl
-         << ob2->name << " " << ob2->age << " " << ob2->roll;
+    int *ptr;
+    int v;
+    ptr = &v;
+    v = (int)ptr;
+    printf("%d\n", v);
+    ptr++;
+    v++;
+    printf("%d %d\n", v, ptr);
 }
