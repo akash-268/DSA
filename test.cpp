2d73ae731a51927f7d107435fd7d1bc1146be09f
akash-268 <aakbajpai268@gmail.com>
2022-09-10 23:34:27 +0530

    temp

--- test.cpp
+++ test.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+struct Node
+{
+    int val;
+    vector<Node*>children;
+    Node():val(0),children({}){}
+    Node(int x):val(x),children({}){}
+    Node(int x,vector<Node*>ch):val(x),children(ch){}
+};
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5};
-    for (auto &i : v)
-    {
-        i = 9;
-        cout << i << " ";
-    }
-    cout << endl;
-    for (auto i : v)
-        cout << i << " ";
+    Node *root =new Node(5);
+    root->children.push_back(new Node(1));
+    root->children.push_back(new Node(2));
+    root->children.push_back(new Node(3));
+    root->children.push_back(new Node(4));
+    root->children[0]->children.push_back(new Node(6));
+    root->children[0]->children.push_back(new Node(7));
+    root->children[0]->children.push_back(new Node(8));
+    root->children[0]->children.push_back(new Node(9));
 }
