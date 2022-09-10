86a02d4513bbc7d70e4a82653f9c1dea751a7331
akash-268 <aakbajpai268@gmail.com>
2022-09-10 12:30:53 +0530

    temp

--- test.cpp
+++ test.cpp
@@ -1,53 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-// JAI SHREE RAM
-// HAR HAR MAHADEV
-#define ll long long int
-#define vin vector<int>
-#define f(i, a, b) for (ll i = a; i < b; i++)
-#define rf(i, a, b) for (ll i = a; i >= b; i--)
-#define mp make_pair
-#define pb push_back
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define mll map<ll, ll>
-#define vc vector
-#define fi first
-#define se second
-#define all(x) x.begin(), x.end()
-const int maxi = 1e5 + 5;
-const ll mod = 1e9 + 7;
-const ll inf = 1e9;
-const ll eps = 1e-9;
-
-void solve()
-{
-    ll n;
-    cin >> n;
-    if (n % 2)
-    {
-        for (int i = n - 3; i >= 2; i--)
-            cout << i << " ";
-        cout << n - 2 << " " << 1 << " " << n - 1 << " " << n << endl;
-    }
-    else
-    {
-
-        for (int i = n - 2; i >= 1; i--)
-            cout << i << " ";
-        cout << n - 1 << " " << n << endl;
-    }
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ll t = 1;
-    cin >> t;
-    while (t--)
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    vector<int> res(n, 0);
+    for (int i = 0; i < n - 1; i++)
     {
-        solve();
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] > v[i])
+            {
+                res[i] = j - i;
+                break;
+            }
+        }
     }
+    for (auto it : res)
+        cout << it << " ";
+    return 0;
 }
