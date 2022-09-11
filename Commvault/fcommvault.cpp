df6a709853916b65dc7497d24d664c2f579781dc
akash-268 <aakbajpai268@gmail.com>
2022-09-11 23:47:28 +0530

    git

--- Commvault/fcommvault.cpp
+++ Commvault/fcommvault.cpp
@@ -1,48 +1,104 @@
 #include <bits/stdc++.h>
 using namespace std;
+class CVFile
+{
+private:
+    string fileName; // poora directory path
+    string content;
+
+public:
+    CVFile(string filePathName, string content);
+    void CVModifyFile(string content);
+    int CVReadFile(int revision, string content);
+    int CVTrim();
+    string getPathName()
+    {
+        return fileName;
+    }
+    string getFileName()
+    {
+        int x = fileName.find_last_of("\\");
+        return fileName.substr(x + 1);
+    }
+};
 class CV_SIOManager
 {
 public:
     CV_SIOManager() {}
-    CVFile *CVGetFile(string StringfilePthName)
-    {
-        return NULL;
-    }
-    void CVDisplayStats()
-    {
-    }
-    int CVDeleteFile(string filePathName)
-    {
-    }
-    int CVMoveFile(string sourcePathName, string targetPathName)
-    {
-    }
-    int CVCopyFile(string sourcePathName, string targetPathName)
-    {
-    }
+    CVFile *CVGetFile(string StringfilePthName);
+    void CVDisplayStats();
+    int CVDeleteFile(string filePathName);
+    int CVMoveFile(string sourcePathName, string targetPathName);
+    int CVCopyFile(string sourcePathName, string targetPathName);
 };
-class CVFile
+unordered_map<string, vector<string>> mp;
+unordered_map<string, CVFile *> m;
+CVFile::CVFile(string filePathName, string content)
 {
-private:
-    string fileName;
-
-public:
-    CVFile()
+    m[filePathName] = this;
+    ofstream f;
+    f.open((string)filePathName);
+    f << content;
+    fileName = filePathName;
+    f.close();
+}
+void CVFile::CVModifyFile(string content)
+{
+    mp[this->getFileName()].push_back(this->content);
+    ofstream f;
+    f.open(fileName);
+    f << content;
+    f.close();
+}
+int CVFile::CVReadFile(int revision, string content)
+{
+    // print content
+    int x = mp[fileName].size() - revision - 1;
+    if (x < 0)
     {
+        cout << "" << endl;
+        return 0;
     }
-    CVFile(string filePathName, string content)
+    content = mp[fileName][x];
+    cout << content << endl;
+    return 0;
+}
+int CVFile::CVTrim()
+{
+    mp[fileName].clear();
+}
+CVFile *CV_SIOManager::CVGetFile(string StringfilePthName)
+{
+    return m[StringfilePthName];
+}
+void CV_SIOManager::CVDisplayStats()
+{
+    for (auto i : mp)
     {
+        cout << "File Name: " << i.first << endl;
+        cout << "Number of revisions: " << i.second.size() << endl;
     }
-    void CVModifyFile(string content)
+}
+int CV_SIOManager::CVDeleteFile(string filePathName)
+{
+    remove(filePathName.c_str());
+}
+int CV_SIOManager::CVMoveFile(string sourcePathName, string targetPathName)
+{
+    rename(sourcePathName.c_str(), targetPathName.c_str());
+}
+int CV_SIOManager::CVCopyFile(string sourcePathName, string targetPathName)
+{
+    ifstream inputFile(sourcePathName);
+    string str;
+    ofstream outputFile(targetPathName);
+    while (getline(inputFile, str))
     {
+        outputFile << str;
     }
-    int CVReadFile(int revision, string content)
-    {
-    }
-    int CVTrim()
-    {
-    }
-};
+    inputFile.close();
+    outputFile.close();
+}
 class SIOManagerDriver
 {
 public:
@@ -81,20 +137,20 @@ public:
         if (NULL != fx)
         {
             fx->CVReadFile(-5, contentInFile);
-            cout << (contentInFile) << endl;
+            // cout << (contentInFile) << endl;
         }
         f4->CVModifyFile("Welcome to Commvault 4!");
         fx = sm->CVGetFile("C:\\File4.txt");
         if (NULL != fx)
         {
             fx->CVReadFile(0, contentInFile);
-            cout << (contentInFile) << endl;
+            // cout << (contentInFile) << endl;
         }
         fx = sm->CVGetFile("C:\\File4.txt");
         if (NULL != fx)
         {
             fx->CVReadFile(-1, contentInFile);
-            cout << (contentInFile) << endl;
+            // cout << (contentInFile) << endl;
         }
         sm->CVDisplayStats();
         sm->CVMoveFile("C:\\File9.txt", "D:\\File30.txt");
