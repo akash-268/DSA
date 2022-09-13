57d1055d72aff87d60958cbdedc60da279ea1ddb
akash-268 <aakbajpai268@gmail.com>
2022-09-13 16:01:45 +0530

    minor changes

--- Commvault/temp.cpp
+++ Commvault/temp.cpp
@@ -3,13 +3,28 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
+
 int main()
 {
-    int x;
-    char s[100];
-    cin >> x;
-    cin.ignore();
-    cin.getline(s, 80);
-    cout << x << endl
-         << s << endl;
+    string s = "I am Akash from Kanpur";
+    s += " ";
+    string ans;
+    stack<string> st;
+    for (auto i = 0; i < s.size(); i++)
+    {
+        ans.push_back(s[i]);
+        if (s[i] == ' ')
+        {
+            st.push(ans);
+            ans = "";
+        }
+    }
+    string ans2;
+    while (!st.empty())
+    {
+        string temp = st.top();
+        ans2 += temp;
+        st.pop();
+    }
+    cout << ans2 << " ";
 }
