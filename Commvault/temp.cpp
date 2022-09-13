09746257abb31c5bf034570c317389189228d514
akash-268 <aakbajpai268@gmail.com>
2022-09-13 21:29:41 +0530

    LIST

--- Commvault/temp.cpp
+++ Commvault/temp.cpp
@@ -3,28 +3,53 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-
+struct Node
+{
+    int data;
+    Node *next;
+    Node() : data(0), next(nullptr) {}
+    Node(int x) : data(x), next(nullptr) {}
+    Node(int x, Node *next) : data(x), next(next) {}
+};
 int main()
 {
-    string s = "I am Akash from Kanpur";
-    s += " ";
-    string ans;
-    stack<string> st;
-    for (auto i = 0; i < s.size(); i++)
+   
+}
+struct Node *reverseList(struct Node *head)
+{
+    struct Node *curr = NULL;
+    while (head)
     {
-        ans.push_back(s[i]);
-        if (s[i] == ' ')
+        struct Node *next = head->next;
+        head->next = curr;
+        curr = head;
+        head = next;
+    }
+    return curr;
+}
+struct Node *AddTwoNumbers(struct Node *list1, struct Node *list2)
+{
+    struct Node *ptr = new Node();
+    struct Node *dummy = ptr;
+    int carry = 0;
+    while (list1 || list2 || carry)
+    {
+        int sum = 0;
+        if (list1 != NULL)
         {
-            st.push(ans);
-            ans = "";
+            sum += list1->data;
+            list1 = list1->next;
         }
+        if (list2 != NULL)
+        {
+            sum += list2->data;
+            list2 = list2->next;
+        }
+        sum += carry;
+        carry = sum / 10;
+        struct Node *node = new Node(sum % 10);
+        ptr->next = node;
+        ptr = ptr->next;
     }
-    string ans2;
-    while (!st.empty())
-    {
-        string temp = st.top();
-        ans2 += temp;
-        st.pop();
-    }
-    cout << ans2 << " ";
+    return reverseList(dummy->next);
 }
