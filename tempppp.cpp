b460e37d62dbe44544efb53dbdb4e42fb4ff8542
akash-268 <aakbajpai268@gmail.com>
2022-09-27 13:32:18 +0530

    minor

--- tempppp.cpp
+++ tempppp.cpp
@@ -1,39 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <bits/stdc++.h>
 using namespace std;
 
-string breakPalindrome(string s)
+bool compareinterval(int i1[], int i2[])
 {
-    string temp = s;
-    bool changed = false;
-    if (s.size() == 1)
-        return "";
-    int i = 0, j = s.size() - 1;
-    bool leftA = true;
-    bool rightA = true;
-    while (i < j)
+    return (i1[0] < i2[0]);
+}
+int breakTime(int input1, int input2[])
+{
+    int i = 0, j = input1 - 1;
+    int id1 = -1, id2 = -1;
+    while (i < input1 - 1)
     {
-        if (s[i] != 'a')
+        if (input2[i + 1] < input2[i])
         {
-            s[i] = 'a';
-            return s;
+            id1 = i;
+            break;
         }
         i++;
+    }
+    while (j >= 0)
+    {
+        if (input2[j - 1] > input2[j])
+        {
+            id2 = j;
+            break;
+        }
         j--;
     }
-    s[s.size() - 1] = 'b';
-    int x = s.compare(temp);
-    if (x)
-        return "IMPOSSIBLE";
-    return s;
+    cout << id1 << " " << id2 << endl;
+    if (id1 != -1 and id2 != -1)
+    {
+        swap(input2[id1], input2[id2]);
+    }
+    int k = 1, sum = 0;
+    for (int i = 0; i < input1; i++)
+    {
+        sum += input2[i] * k++;
+    }
+    return sum;
 }
 
 int main()
 {
-
-    cout << breakPalindrome("aaa");
+    int arr[5] = {3, 1, 6, 3, 1};
+    cout << breakTime(5, arr);
 
     return 0;
 }
