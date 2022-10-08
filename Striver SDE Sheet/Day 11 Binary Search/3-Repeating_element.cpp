73d5490831fccd25361404af2146aba43256fb8e
akash-268 <aakbajpai268@gmail.com>
2022-10-09 01:59:46 +0530

    Repeating Element

---
+++ Striver SDE Sheet/Day 11 Binary Search/3-Repeating_element.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin vector<int>
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int singleNonDuplicate(vector<int> &nums)
+{
+    int i = 0, j = nums.size() - 2;
+    while (i <= j)
+    {
+        int mid = (i + j) / 2;
+        if (mid % 2)
+        {
+            if (nums[mid - 1] == nums[mid])
+            {
+                i = mid + 1;
+            }
+            else
+                j = mid - 1;
+        }
+        else
+        {
+            if (nums[mid] == nums[mid + 1])
+                i = mid + 1;
+            else
+                j = mid - 1;
+        }
+    }
+    return nums[i];
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
