96b66d3269cf4a3ba4263eaa710f0a84b57560fe
akash-268 <aakbajpai268@gmail.com>
2022-10-09 02:31:14 +0530

    Search in a sorted and rotated array

--- Striver SDE Sheet/Day 11 Binary Search/4-Search_in_sorted_rotated_array.cpp
+++ Striver SDE Sheet/Day 11 Binary Search/4-Search_in_sorted_rotated_array.cpp
@@ -39,7 +39,31 @@ struct TreeNode
 void solve()
 {
 }
-
+int search(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[low] <= nums[mid])
+        {
+            if (target >= nums[low] && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            if (target > nums[mid] && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
