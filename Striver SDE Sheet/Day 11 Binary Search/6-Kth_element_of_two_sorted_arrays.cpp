73f90c58057272f4c6aaed9715d9cbc65ae49be2
akash-268 <aakbajpai268@gmail.com>
2022-10-09 17:51:22 +0530

    Kth Element of two sorted arrays

--- Striver SDE Sheet/Day 11 Binary Search/6-Kth_element_of_two_sorted_arrays.cpp
+++ Striver SDE Sheet/Day 11 Binary Search/6-Kth_element_of_two_sorted_arrays.cpp
@@ -39,7 +39,34 @@ struct TreeNode
 void solve()
 {
 }
+int kthElement(int nums1[], int nums2[], int n, int m, int k)
+{
+    int n1 = n;
+    int n2 = m;
+    if (n2 < n1)
+    {
+        return kthElement(nums2, nums1, n2, n1, k);
+    }
+    int start = max(0, k - n2), end = min(n1, k);
+    while (start <= end)
+    {
+        int cut1 = (start + end) / 2;
+        int cut2 = k - cut1;
+        int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+        int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+        int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+        int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+        if (left1 <= right2 && left2 <= right1)
+        {
 
+            return max(left1, left2);
+        }
+        else if (left1 > right2)
+            end = cut1 - 1;
+        else
+            start = cut1 + 1;
+    }
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
