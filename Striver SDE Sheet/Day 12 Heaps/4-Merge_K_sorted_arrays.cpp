f8720ff2853555bff92f6f2cd427077532a5feea
akash-268 <aakbajpai268@gmail.com>
2022-10-19 00:28:23 +0530

    merge K sorted Arrays

---
+++ Striver SDE Sheet/Day 12 Heaps/4-Merge_K_sorted_arrays.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+// JAI SHREE RAM
+// HAR HAR MAHADEV
+#define ll long long int
+#define vin vector<int>
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, a, b) for (ll i = a; i >= b; i--)
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define vc vector
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+const int maxi = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9;
+const ll eps = 1e-9;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x),
+                      left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+vector<int> mergeKSortedArrays(vector<vector<int>> &v, int k)
+{
+    // Write your code here.
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (auto it : v)
+    {
+        for (auto i : it)
+            pq.push(i);
+    }
+    vector<int> ans;
+    while (!pq.empty())
+    {
+        int a = pq.top();
+        ans.push_back(a);
+        pq.pop();
+    }
+    return ans;
+}
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
