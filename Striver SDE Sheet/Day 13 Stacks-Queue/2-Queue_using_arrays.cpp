165f7d0af3ba081008d846d47d01ed7bc1b443b2
akash-268 <aakbajpai268@gmail.com>
2022-10-19 16:44:08 +0530

    Queue using Arrays - CORE LOGIC

---
+++ Striver SDE Sheet/Day 13 Stacks-Queue/2-Queue_using_arrays.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+class Queue
+{
+    int *arr;
+    int fron, rear, size;
+
+public:
+    Queue()
+    {
+        // Implement the Constructor
+        fron = 0;
+        rear = 0;
+        size = 100001;
+        arr = new int[size];
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty()
+    {
+        // Implement the isEmpty() function
+        if (fron == rear)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void enqueue(int data)
+    {
+        // Implement the enqueue() function
+        if (rear == size)
+            cout << "FULL";
+        else
+            arr[rear++] = data;
+    }
+
+    int dequeue()
+    {
+        // Implement the dequeue() function
+        if (fron == rear)
+            return -1;
+        else
+        {
+            int a = arr[fron++];
+            if (fron == rear)
+            {
+                fron = 0;
+                rear = 0;
+            }
+            return a;
+        }
+    }
+
+    int front()
+    {
+        // Implement the front() function
+        if (!(fron == rear))
+            return arr[fron];
+        return -1;
+    }
+};
