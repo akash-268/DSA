abfe5fdaa24306e19c95b3271c795e43a51d661d
akash-268 <aakbajpai268@gmail.com>
2022-11-06 23:37:17 +0530

    Delloite question 1

--- test.cpp
+++ test.cpp
@@ -2,63 +2,32 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-#define ll long long int
-#define f(i, a, b) for (ll i = a; i < b; i++)
-#define rf(i, a, b) for (ll i = a; i >= b; i--)
-#define mp make_pair
-#define pb push_back
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define mll map<ll, ll>
-#define vc vector
-#define fi first
-#define se second
-#define all(x) x.begin(), x.end()
-void fun(vector<pair<bool, ll>> &vec, ll &n, vector<ll> &v, ll &res)
+int solve(string s)
 {
-    vec.pb({v[1], 1});
-    for (ll i = 2; i < n; ++i)
-        if (v[i] == vec[vec.size() - 1].fi)
-            vec[vec.size() - 1].se++;
-        else
-            vec.pb({v[i], 1});
-    vec.pb({0, 0});
-    for (ll i = 0; i < vec.size() - 1; ++i)
-        res += vec[i].se * (vec[i].se + 1) / 2;
-    for (ll i = 0; i < vec.size() - 1; ++i)
-        if (vec[i].fi)
-            res += vec[i].se * vec[i + 1].se;
-}
-void solve()
-{
-    ll n, res;
-    cin >> n;
-    vector<ll> a(n);
-    vector<ll> b(n);
-    res = 0;
-    for (ll i = 0; i < n; ++i)
-        cin >> a[i];
-    if (n == 1)
+    int n = s.size();
+    vector<int> vis(n, 1);
+    for (int i = 0; i < n; i++)
     {
-        cout << "1" << endl;
-        return;
+        if (s[i] == 'X')
+            vis[i] = 0;
     }
-    for (ll i = 1; i < n; ++i)
-        b[i] = a[i] < a[i - 1];
-    vector<pair<bool, ll>> vec;
-    fun(vec, n, b, res);
-    cout << res + n << endl;
+    int c = 0;
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (s[i] == 'X' && vis[i] == 0)
+        {
+            c++;
+            vis[i] = 1;
+            vis[i + 1] = 1;
+            vis[i + 2] = 1;
+        }
+    }
+    return c;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ll t = 1;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
+    string s;
+    cin >> s;
+    cout << solve(s);
 }
