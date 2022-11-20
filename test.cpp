a1da87177c09ff99e116b3f3be137e795a446452
akash-268 <aakbajpai268@gmail.com>
2022-11-20 14:12:03 +0530

    minor

--- test.cpp
+++ test.cpp
@@ -2,35 +2,50 @@
 using namespace std;
 // JAI SHREE RAM
 // HAR HAR MAHADEV
-int solve(string s)
-{
-    int n = s.size();
-    vector<int> vis(n, 1);
-    for (int i = 0; i < n; i++)
+// int binarySearchCount(int arr[], int n, int key)
+// {
+//     int left = 0;
+//     int right = n - 1;
+
+//     int count = 0;
+
+//     while (left <= right)
+//     {
+//         int mid = (right + left) / 2;
+//         if (arr[mid] <= key)
+//         {
+//             count = mid + 1;
+//             left = mid + 1;
+//         }
+//         else
+//             right = mid - 1;
+//     }
+
+//     return count;
+// }
+
+// int solve(vector<int> cost,int n,vector<int>e1,vector<int>e2){
+//     vector<pair<int,int>>adj[n];
+//     for(int i=0;i<cost.size();i++){
+//         adj[e1[i]].push_back({e2[i],cost[i]});
+//     }
+    
+// }
+
+int maxPerksItems(int catridges,int dollars,int recycleReward,int perksCost){
+    int ans = min(catridges, dollars / perksCost);
+    while (catridges > 0)
     {
-        if (s[i] == 'X')
-            vis[i] = 0;
+        catridges--;
+        dollars += recycleReward;
+        ans = max(ans, min(catridges, dollars / perksCost));
     }
-    int c = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (s[i] == 'X' && vis[i] == 0)
-        {
-            c++;
-            if (i < n)
-                vis[i] = 1;
-            if (i + 1 < n)
-                vis[i + 1] = 1;
-            if (i + 2 < n)
-                vis[i + 2] = 1;
-        }
-    }
-    return c;
+    return ans;
 }
 
 int main()
 {
-    string s;
-    cin >> s;
-    cout << solve(s);
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout << maxPerksItems(a, b, c, d);
 }
